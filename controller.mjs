8a093f4c9b4cc5d376702af06f66570e3658f242
dscottdiperna <dscottdiperna@gmail.com>
2023-01-01 16:20:46 -0500

    update .gitignore

--- controller.mjs
+++ controller.mjs
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// const path = require("path");
-
 app.use(express.json());
 
 app.use(express.static(path.join("simply-deapan", "client", "build")));
